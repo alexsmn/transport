aabb90225edf1f86ad41cd837bd01606af49e722
alexsmn <alexsmn@gmail.com>
2020-12-05 18:28:35 -0700

    Add TransportMock.

---
+++ net/transport_mock.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "net/transport.h"
+
+#include <gmock/gmock.h>
+
+namespace net {
+
+class TransportMock : public Transport {
+ public:
+  MOCK_METHOD1(Open, Error(Delegate& delegate));
+  MOCK_METHOD0(Close, void());
+  MOCK_METHOD2(Read, int(void* data, size_t len));
+  MOCK_METHOD2(Write, int(const void* data, size_t len));
+  MOCK_CONST_METHOD0(GetName, std::string());
+  MOCK_CONST_METHOD0(IsMessageOriented, bool());
+  MOCK_CONST_METHOD0(IsConnected, bool());
+  MOCK_CONST_METHOD0(IsActive, bool());
+};
+
+}  // namespace net
