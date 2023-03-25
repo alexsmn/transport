5e9cdf1bf39850438c6e46fda94b0a9daf9cd135
alexsmn <alexsmn@gmail.com>
2023-03-25 14:22:45 -0700

    Add WebSocket UT.

---
+++ net/websocket_transport_unittest.cpp
@@ -0,0 +1,24 @@
+#include "net/websocket_transport.h"
+
+#include "net/transport_delegate_mock.h"
+
+#include <boost/asio/io_context.hpp>
+#include <gmock/gmock.h>
+
+using namespace testing;
+
+namespace net {
+
+class WebSocketTransportTest : public Test {};
+
+TEST_F(WebSocketTransportTest, Test) {
+  boost::asio::io_context io_context;
+
+  int port = 4000;
+
+  TransportDelegateMock server_delegate;
+  WebSocketTransport server{io_context, {}, port};
+  ASSERT_EQ(OK, server.Open(server_delegate));
+}
+
+}  // namespace net
