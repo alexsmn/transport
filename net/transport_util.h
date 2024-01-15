35f2c5bb5fd9c4b27451d7c18008fe0ed1c852c3
alexsmn <alexsmn@gmail.com>
2024-01-15 11:14:25 -0800

    Cleanup net::connector interface.

--- net/transport_util.h
+++ net/transport_util.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "net/opened_transport.h"
 #include "net/transport.h"
 
 namespace net {
@@ -36,47 +35,6 @@ inline std::pair<promise<void>, Connector::Handlers> MakePromiseHandlers(
   };
 
   return {state->p, std::move(promise_handlers)};
-}
-
-inline promise<opened_transport> OpenWithPromise(
-    std::unique_ptr<Transport> transport,
-    const Connector::Handlers& handlers) {
-  if (!transport) {
-    return make_error_promise<opened_transport>(ERR_INVALID_HANDLE);
-  }
-
-  struct State {
-    std::unique_ptr<Transport> transport;
-    bool done = false;
-    promise<opened_transport> p;
-  };
-
-  auto state = std::make_shared<State>(std::move(transport));
-
-  auto promise_handlers = handlers;
-
-  promise_handlers.on_open = [state, original_on_open = handlers.on_open] {
-    original_on_open();
-
-    if (!state->done) {
-      state->done = true;
-      state->p.resolve(opened_transport{std::move(state->transport)});
-    }
-  };
-
-  promise_handlers.on_close = [state, original_on_close =
-                                          handlers.on_close](Error error) {
-    original_on_close(error);
-
-    if (!state->done) {
-      state->done = true;
-      state->p.reject(error);
-    }
-  };
-
-  state->transport->Open(promise_handlers);
-
-  return state->p;
 }
 
 }  // namespace net
