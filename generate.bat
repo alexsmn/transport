3f8b8523851894708da017f74b4919ef367765c2
alexsmn <alexsmn@gmail.com>
2024-04-14 09:09:45 -0700

    Build fix.

--- generate.bat
+++ generate.bat
@@ -1,1 +1,3 @@
-cmake -B build -S . --toolchain "d:\vcpkg\scripts\buildsystems\vcpkg.cmake"
+cmake -B build -A Win32 -S . ^
+  --toolchain "d:\vcpkg\scripts\buildsystems\vcpkg.cmake" ^
+  -DCMAKE_MODULE_PATH:PATH="d:\TC\third_party\chromebase;d:\TC\third_party\promise.hpp"
