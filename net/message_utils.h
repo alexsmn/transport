a1991ef111bbccb28296c8c8c0c7d50e2a32e9d0
alexsmn <alexsmn@gmail.com>
2024-07-06 13:13:43 -0700

    Cleanup.

--- net/message_utils.h
+++ net/message_utils.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "net/any_transport.h"
 #include "net/bytemsg.h"
+#include "net/transport.h"
 
 #include <stdexcept>
 #include <string>
@@ -25,4 +27,25 @@ inline unsigned short SwapBytesInWord(unsigned short value) {
   return ((value & 0x00FF) << 8) | ((value & 0xFF00) >> 8);
 }
 
-} // namespace net
+// Empty result buffer means connection is closed.
+inline awaitable<Error> ReadMessage(Transport& transport,
+                                    size_t max_size,
+                                    std::vector<char>& buffer) {
+  buffer.resize(max_size);
+  NET_ASSIGN_OR_CO_RETURN(auto bytes_read, co_await transport.Read(buffer));
+  buffer.resize(bytes_read);
+  co_return OK;
+}
+
+inline awaitable<Error> ReadMessage(any_transport& transport,
+                                    size_t max_size,
+                                    std::vector<char>& buffer) {
+  auto* impl = transport.get_impl();
+  if (!impl) {
+    co_return ERR_INVALID_HANDLE;
+  }
+
+  co_return co_await ReadMessage(*impl, max_size, buffer);
+}
+
+}  // namespace net
