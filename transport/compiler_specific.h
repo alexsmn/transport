9a013a756e30972175a1e0fc16ded1d6534fba73
alexsmn <alexsmn@gmail.com>
2024-08-31 11:53:11 -0700

    Rename compiler definitions.

--- transport/compiler_specific.h
+++ transport/compiler_specific.h
@@ -1,20 +1,20 @@
 #pragma once
 
 #if defined(__clang__)
-#define COMPILER_CLANG
+#define TRANSPORT_COMPILER_CLANG
 #elif defined(__GNUC__) || defined(__GNUG__)
-#define COMPILER_GCC
+#define TRANSPORT_COMPILER_GCC
 #elif defined(_MSC_VER)
-#define COMPILER_MSVC
+#define TRANSPORT_COMPILER_MSVC
 #endif
 
-#if defined(COMPILER_GCC) || defined(COMPILER_CLANG)
+#if defined(TRANSPORT_COMPILER_GCC) || defined(TRANSPORT_COMPILER_CLANG)
 #define PRINTF_FORMAT(format_param, dots_param) \
   __attribute__((format(printf, format_param, dots_param)))
 #else
 #define PRINTF_FORMAT(format_param, dots_param)
 #endif
 
-#if !defined(COMPILER_MSVC)
+#if !defined(TRANSPORT_COMPILER_MSVC)
 #define _Printf_format_string_
 #endif
